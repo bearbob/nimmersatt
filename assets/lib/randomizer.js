1a7ce220822622c6d57869800f9c337a42826549
Björn Schmidt <bearbob@users.noreply.github.com>
2022-01-04 23:30:01 +0100

    Initial push

---
+++ assets/lib/randomizer.js
@@ -0,0 +1,7 @@
+function getRandom() {
+  var p = document.getElementById("content");
+
+  const random = Math.floor(Math.random() * raw_data.length);
+
+  p.innerHTML = raw_data[random].name;
+};
---
+++ data/recipes.js
@@ -0,0 +1,10 @@
+const raw_data = [
+  {
+    "name": "Bierbrot",
+    "tags": ["bread", "baking", "beer"]
+  },
+  {
+    "name": "Milchreis",
+    "tags": ["rice", "sweet", "milk"]
+  }
+];
