7498c2e22a2a5d2050004a18c9002c050914e8ce
Björn Schmidt <bearbob@users.noreply.github.com>
2022-01-05 22:51:23 +0100

    Added more sample recipes

--- data/recipes.js
+++ data/recipes.js
@@ -12,5 +12,24 @@ const raw_data = [
     "name": "Black-eyed bean mole with salsa",
     "tags": ["rice", "sweet", "milk", "beans", "salsa"],
     "link": "https://www.bbcgoodfood.com/recipes/black-eye-bean-mole-salsa"
+  },
+  {
+    "name": "Auberginencurry",
+    "link": "https://www.lidl-kochen.de/rezeptwelt/auberginen-curry-268581"
+  },
+  {
+    "name": "Bananenbrot"
+  },
+  {
+    "name": "Kung Pao Chicken",
+    "link": "https://www.edeka.de/rezepte/rezept/kung-pao-chicken.jsp",
+    "video": "https://www.youtube.com/watch?v=nKOpKflSNkc"
+  },
+  {
+    "name": "Eierreis"
+  },
+  {
+    "name": "One-Pot-Paste mit Zitronenkäsesauce",
+    "link": "https://www.penny.de/erleben/rezepte-und-ernaehrung/one-pot-pasta-mit-zitronen-kaeseso%C3%9Fe"
   }
 ];
