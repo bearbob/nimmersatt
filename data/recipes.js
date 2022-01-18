7e68231a1b5c2049f03199ebcd11d57d1dc5a7f5
Björn Schmidt <bearbob@users.noreply.github.com>
2022-01-18 14:12:08 +0100

    Update recipes.js

--- data/recipes.js
+++ data/recipes.js
@@ -1,7 +1,7 @@
 const books = {
   "jamies5zutaten": {
     "name": "Jamies 5-Zutaten-Küche",
-    "ISBN": "978-3-8310-3422-5"
+    "ISBN": "978-3-8310-3422-5",
   }
 };
 const recipes = [
@@ -9,184 +9,184 @@ const recipes = [
     "name": "Wildreis mit Mangold und Kirschen",
     "book": {
       "id": "jamies5zutaten",
-      "page": 32
-    }
+      "page": 32,
+    },
   },
   {
     "name": "Geniale Grüne Spaghetti",
     "subtitle": "mit Schwarzkohl und Ricotta",
     "book": {
       "id": "jamies5zutaten",
-      "page": 50
-    }
+      "page": 50,
+    },
   },
   {
     "name": "Blitzschnelle Bratwurst-Carbonara",
     "book": {
       "id": "jamies5zutaten",
-      "page": 52
-    }
+      "page": 52,
+    },
   },
   {
     "name": "Pasta mit Hackfleisch und Steinpilzen",
     "book": {
       "id": "jamies5zutaten",
-      "page": 68
-    }
+      "page": 68,
+    },
   },
   {
     "name": "Koreanischer Eierreis",
     "book": {
       "id": "jamies5zutaten",
-      "page": 80
-    }
+      "page": 80,
+    },
   },
   {
     "name": "Frittata mit Pilzen und Räucherspeck",
     "book": {
       "id": "jamies5zutaten",
-      "page": 92
-    }
+      "page": 92,
+    },
   },
   {
     "name": "Knuspriges Knoblauchhähnchen",
     "book": {
       "id": "jamies5zutaten",
-      "page": 108
-    }
+      "page": 108,
+    },
   },
   {
     "name": "Schnelle Asia-Frikadellen",
     "book": {
       "id": "jamies5zutaten",
-      "page": 130
-    }
+      "page": 130,
+    },
   },
   {
     "name": "Erbsen, dicke Bohnen, Chili & Minze",
     "book": {
       "id": "jamies5zutaten",
-      "page": 176
+      "page": 176,
     },
-    "link": "https://www.jamieoliver.com/recipes/vegetable-recipes/peas-beans-chilli-mint/"
+    "link": "https://www.jamieoliver.com/recipes/vegetable-recipes/peas-beans-chilli-mint/",
   },
   {
     "name": "Bierbrot",
     "tags": ["brot", "bread", "baking", "beer"],
-    "link": "https://www.marcelpaa.com/rezepte/bierbrot/"
+    "link": "https://www.marcelpaa.com/rezepte/bierbrot/",
   },
   {
     "name": "Sonnenblumenbrot",
     "tags": ["brot", "bread", "baking"],
     "link": "https://www.marcelpaa.com/rezepte/sonnenblumenbrot/",
-    "video": "https://www.youtube.com/watch?v=WNZV6t9_FIw"
+    "video": "https://www.youtube.com/watch?v=WNZV6t9_FIw",
   },
   {
     "name": "Urdinkel Schlumberger",
     "tags": ["brot", "bread", "baking"],
     "link": "https://www.marcelpaa.com/rezepte/urdinkel-schlumberger/",
-    "video": "https://www.youtube.com/watch?v=zpvk80lLhs8"
+    "video": "https://www.youtube.com/watch?v=zpvk80lLhs8",
   },
   {
     "name": "Roggen-Vollkornbrötchen",
     "tags": ["brot", "bread", "baking"],
     "link": "https://www.marcelpaa.com/rezepte/roggen-vollkornbroetchen/",
-    "video": "https://www.youtube.com/watch?v=f2u9ZcHiMYQ"
+    "video": "https://www.youtube.com/watch?v=f2u9ZcHiMYQ",
   },
   {
     "name": "Kartoffelkiste",
     "tags": ["brot", "bread", "baking"],
     "link": "https://www.marcelpaa.com/rezepte/kartoffelkiste/",
-    "video": "https://www.youtube.com/watch?v=GyUhTJhI45c"
+    "video": "https://www.youtube.com/watch?v=GyUhTJhI45c",
   },
   {
     "name": "Rustikales Mischbrot",
     "link": "https://www.ploetzblog.de/2021/02/20/rustikales-mischbrot/",
-    "tags": ["brot", "bread", "baking"]
+    "tags": ["brot", "bread", "baking"],
   },
   {
     "name": "Dinkelvollkornmischbrot mit 25% Gerstenkörnern",
     "link": "https://www.ploetzblog.de/2022/01/01/dinkelvollkornmischbrot-mit-25-gerstenkoernern/",
-    "tags": ["brot", "bread", "baking"]
+    "tags": ["brot", "bread", "baking"],
   },
   {
     "name": "Rustikales Gewürz-Vollkornbrot",
     "subtitle": "perfekt für die Sauerteig-Resteverwertung",
     "link": "https://madamroteruebe.de/2022/01/gewuerz-vollkornbrot-perfekt-fuer-die-sauerteig-resteverwertung/",
-    "tags": ["brot", "bread", "baking"]
+    "tags": ["brot", "bread", "baking"],
   },
   {
     "name": "Milchreis",
-    "tags": ["rice", "sweet", "milk", "reis"]
+    "tags": ["rice", "sweet", "milk", "reis"],
   },
   {
     "name": "Black-eyed bean mole with salsa",
     "tags": ["rice", "sweet", "milk", "beans", "salsa"],
-    "link": "https://www.bbcgoodfood.com/recipes/black-eye-bean-mole-salsa"
+    "link": "https://www.bbcgoodfood.com/recipes/black-eye-bean-mole-salsa",
   },
   {
     "name": "Auberginencurry",
-    "link": "https://www.lidl-kochen.de/rezeptwelt/auberginen-curry-268581"
+    "link": "https://www.lidl-kochen.de/rezeptwelt/auberginen-curry-268581",
   },
   {
-    "name": "Bananenbrot"
+    "name": "Bananenbrot",
   },
   {
     "name": "Kung Pao Chicken",
     "link": "https://www.edeka.de/rezepte/rezept/kung-pao-chicken.jsp",
-    "video": "https://www.youtube.com/watch?v=nKOpKflSNkc"
+    "video": "https://www.youtube.com/watch?v=nKOpKflSNkc",
   },
   {
-    "name": "Eierreis"
+    "name": "Eierreis",
   },
   {
     "name": "One-Pot-Paste mit Zitronenkäsesauce",
-    "link": "https://www.penny.de/erleben/rezepte-und-ernaehrung/one-pot-pasta-mit-zitronen-kaeseso%C3%9Fe"
+    "link": "https://www.penny.de/erleben/rezepte-und-ernaehrung/one-pot-pasta-mit-zitronen-kaeseso%C3%9Fe",
   },
   {
     "name": "Fischstäbchen",
-    "subtext": "mit Buttergemüse und Kartoffelbrei oder Erbsen und Reis"
+    "subtext": "mit Buttergemüse und Kartoffelbrei oder Erbsen und Reis",
   },
   {
-    "name": "Kartoffelsuppe"
+    "name": "Kartoffelsuppe",
   },
   {
     "name": "Sheperds Pie",
-    "link": "https://www.jamieoliver.com/recipes/lamb-recipes/super-shepherd-s-pie/"
+    "link": "https://www.jamieoliver.com/recipes/lamb-recipes/super-shepherd-s-pie/",
   },
   {
-    "name": "Pizza"
+    "name": "Pizza",
   },
   {
-    "name": "Grießbrei"
+    "name": "Grießbrei",
   },
   {
-    "name": "Flammkuchen"
+    "name": "Flammkuchen",
   },
   {
-    "name": "Käsespätzle"
+    "name": "Käsespätzle",
   },
   {
-    "name": "Quarkeierkuchen"
+    "name": "Quarkeierkuchen",
   },
   {
-    "name": "Ofengemüse"
+    "name": "Ofengemüse",
   },
   {
     "name": "Gerösteter Curry-Blumenkohl",
     "subtitle": "mit frischer Zitronen-Raita",
     "link": "https://www.eat-this.org/curry-blumenkohl-mit-zitronen-raita/",
-    "tags": ["rice", "curry", "blumenkohl", "reis"]
+    "tags": ["rice", "curry", "blumenkohl", "reis"],
   },
   {
     "name": "Cremige Süßkartoffelsuppe",
     "subtitle": "mit rauchigen Rosenkohl-Chips",
-    "link": "https://www.eat-this.org/cremige-suesskartoffelsuppe-mit-rauchigen-rosenkohl-chips/"
+    "link": "https://www.eat-this.org/cremige-suesskartoffelsuppe-mit-rauchigen-rosenkohl-chips/",
   },
   {
     "name": "Pasta e fagioli",
     "subtitle": "das bohnigste Pastagericht überhaupt",
     "link": "https://www.eat-this.org/vegane-pasta-e-fagioli/",
-    "tags": ["pasta", "noodles"]
-  }
+    "tags": ["pasta", "noodles"],
+  },
 ];
